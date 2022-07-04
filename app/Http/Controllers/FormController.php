56f5f8e351a2b0a34831aa086b936c91ce9c474f
Shell-by <wnsghks1026@naver.com>
2022-07-04 11:46:32 +0900

    1

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -78,49 +78,4 @@ class FormController extends Controller
 
         return view('recordInput')->with('request', $request)->with('form_id', $form_id);
     }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
